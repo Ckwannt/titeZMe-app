04c9678a7e690f9ff559346ed6ed48d541c7b25f
Ckwannt <hxhsv628y2@privaterelay.appleid.com>
2026-05-03 15:34:59 +0200

    feat: Add image hostnames to next.config.js
    Allow fetching images from Firebase Storage and Googleusercontent by adding their hostnames to the `next.config.js` image domains.

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,24 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**', // This allows any path under the hostname
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   output: 'standalone',
