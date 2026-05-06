c72348aa37bd4e085f7095816c964fe2b61108d9
Ckwannt <hxhsv628y2@privaterelay.appleid.com>
2026-05-06 06:51:03 +0100

    feat: Improve booking flow and navigation
    Refactors the booking page to better handle user profiles and shop/solo bookings. Updates the homepage to link to a dedicated search page instead of an anchor.

--- app/book/[barberId]/page.tsx
+++ app/book/[barberId]/page.tsx
@@ -40,12 +40,9 @@ export default function BookingPage({ params }: { params: Promise<{ barberId: st
   useEffect(() => {
     if (profile && step === 1) {
       if (profile.isSolo && profile.shopId) {
-        // eslint-disable-next-line react-hooks/set-state-in-effect
         setStep(1);
       } else {
-        // eslint-disable-next-line react-hooks/set-state-in-effect
         setBookingContext(profile.shopId && !profile.isSolo ? 'shop' : 'solo');
-        // eslint-disable-next-line react-hooks/set-state-in-effect
         setStep(2);
       }
     }
